402be85e40471815662faf17a7da14b135f8ad73
Meph384 <jakub.kozak384@gmail.com>
2024-04-27 20:14:51 +0200

    feat: hero + about + sidenav add: material refactor: file structure for styles

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeroComponent} from "./components/hero/hero.component";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {AboutComponent} from "./components/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent, MatIcon, MatIconButton, MatSidenav, MatSidenavContainer, MatSidenavContent, MatListItem, MatNavList, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ frontend/src/app/components/hero/hero.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {animate, style, transition, trigger} from "@angular/animations";
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [
+    MatButton
+  ],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1000ms ease-out', style({ opacity: 1 }))
+      ])
+    ])
+  ]
+})
+export class HeroComponent {
+
+}
