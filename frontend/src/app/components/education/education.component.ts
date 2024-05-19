2d723c87bda5b563d36846465fe077112024382c
Dominik Gawlak <d.gawlak@softint.pl>
2024-05-19 18:20:58 +0200

    certificates

--- frontend/src/app/components/education/education.component.ts
+++ frontend/src/app/components/education/education.component.ts
@@ -29,23 +29,95 @@ import {MatIcon} from "@angular/material/icon";
 export class EducationComponent {
   items = [
     {
-      name: 'Certified Angular Developer',
-      description: 'Certification demonstrating advanced Angular skills.',
-      imageUrl: 'https://marketplace.canva.com/EAFNlUJs5g4/2/0/1600w/canva-white-simple-certificate-of-appreciation-Fcz7KkZ5YaU.jpg',
+      name: 'Certyfikat "Analiza i interpretacja planów treningowych - lower body"',
+      description: '',
+      imageUrl: './assets/images/certificates/certificate analiza i interpretacja planow treningowy - lower body.jpg',
       state: 'expanded'
     },
     {
-      name: 'Frontend Development with Angular',
-      description: 'Learn to build dynamic and responsive web applications using Angular.',
-      imageUrl: '',
+      name: 'Certyfikat "Analiza i interpretacja planów treningowych - upper body"',
+      description: '',
+      imageUrl: './assets/images/certificates/certificate analiza i interpretacja planow treningowych - upper body.jpg',
       state: 'collapsed'
     },
     {
-      name: 'Advanced Angular Techniques',
-      description: 'Dive deeper into Angular with advanced components and directives.',
-      imageUrl: '',
+      name: 'Certyfikat IPSS',
+      description: '',
+      imageUrl: './assets/images/certificates/Certifikat IPSS.png',
       state: 'collapsed'
-    }
+    },
+    {
+      name: 'Certyfikat "Dietetyka sportowa"',
+      description: '',
+      imageUrl: './assets/images/certificates/certificate dietetyka sportowa.jpg',
+      state: 'collapsed'
+    },
+    {
+      name: 'Certyfikat "Programowanie treningowe w praktyce"',
+      description: '',
+      imageUrl: './assets/images/certificates/certificate programowanie treningowe w praktyce.jpg',
+      state: 'collapsed'
+    },
+    {
+      name: 'Certyfikat "Programowanie treningu siłowego 2023"',
+      description: '',
+      imageUrl: './assets/images/certificates/Programowanie Treningu silowego.jpg',
+      state: 'collapsed'
+    },
+    {
+      name: 'Certyfikat "Prowadzenie dietetyczne krok po kroku"',
+      description: '',
+      imageUrl: './assets/images/certificates/certificate prowadzenie dietetyczne krok po kroku.jpg',
+      state: 'collapsed'
+    },
+    {
+      name: 'Certyfikat "Testy funkcjonalne"',
+      description: '',
+      imageUrl: './assets/images/certificates/certificate Testy funkcjonalne.jpg',
+      state: 'collapsed'
+    },
+    {
+      name: 'Certyfikat "Strategie doboru ćwiczeń"',
+      description: '',
+      imageUrl: './assets/images/certificates/Certifikat-strategie-doboru-cwiczen.jpg',
+      state: 'collapsed'
+    },
+    {
+      name: 'Certyfikat "Vademecum Pośladków część 1"',
+      description: '',
+      imageUrl: './assets/images/certificates/Certifikat-vademecum-posladkow-czesc-1.jpg',
+      state: 'collapsed'
+    },
+    {
+      name: 'Certyfikat "Vademecum Pośladków część 2"',
+      description: '',
+      imageUrl: './assets/images/certificates/Certifikat-vademecum-posladkow-czesc-2.jpg',
+      state: 'collapsed'
+    },
+    {
+      name: 'Certyfikat "Vademecum Pośladków część 3"',
+      description: '',
+      imageUrl: './assets/images/certificates/Certifikat-vademecum-posladkow-czesc-3.png',
+      state: 'collapsed'
+    },
+    {
+      name: 'Certyfikat "Tworzenie planu treningowego od podstaw"',
+      description: '',
+      imageUrl: './assets/images/certificates/Tworzenie planu treningowe od podstaw.png',
+      state: 'collapsed'
+    },
+    {
+      name: 'Certyfikat "Historia niejednego pośladka"',
+      description: '',
+      imageUrl: './assets/images/certificates/Historia Niejednego posladka.jpg',
+      state: 'collapsed'
+    },
+    {
+      name: 'Certyfikat "Programowanie treningu siłowego 2020"',
+      description: '',
+      imageUrl: './assets/images/certificates/certificate programowanie treningu silowego.jpg',
+      state: 'collapsed'
+    },
   ];
 
   toggle(item: any): void {
