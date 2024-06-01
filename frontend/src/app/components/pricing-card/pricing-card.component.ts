004e055c8bfbe7776e30ff2cde45d42b7decf90d
Dominik Gawlak <d.gawlak@softint.pl>
2024-06-01 18:10:24 +0200

    changes description for offer

--- frontend/src/app/components/pricing-card/pricing-card.component.ts
+++ frontend/src/app/components/pricing-card/pricing-card.component.ts
@@ -12,29 +12,29 @@ export class PricingCardComponent {
     {
       backgroundImage: 'url("./assets/images/offer/Trening personalny.jpg")',
       title: 'Treningi personalne',
-      shortDescription: 'Ideal for beginners and advanced.',
-      longDescription: 'This plan includes 5 sessions per month, nutrition guidance, and 24/7 gym access.',
+      shortDescription: 'krótki opis od razu widoczny',
+      longDescription: 'dłuższy tekst po najechaniu',
       price: 'Umów się na bezpłatną konsultację'
     },
     {
       backgroundImage: 'url("./assets/images/offer/Trening w parach.jpg")',
       title: 'Treningi w parach',
-      shortDescription: 'Ideal for couples.',
-      longDescription: 'This plan includes 5 sessions per month, nutrition guidance, and 24/7 gym access.',
+      shortDescription: 'krótki opis od razu widoczny',
+      longDescription: 'dłuższy tekst po najechaniu.',
       price: 'Umów się na bezpłatną konsultację'
     },
     {
       backgroundImage: 'url("./assets/images/offer/Indywidualny plan treningowy.jpg")',
       title: 'Indywidualny Plan Treningowy',
-      shortDescription: 'Ideal for couples.',
-      longDescription: 'This plan includes 7 sessions per month, nutrition guidance, and 24/7 gym access.',
+      shortDescription: 'Spersonalizowny plan treningowy',
+      longDescription: 'Przygotuję dla Ciebie plan dostosowany do Twoich potrzeb z uwzględnieniem Twojego stylu życia, nawyków żywieniowych i czasu który jesteś w stanie poświęcić na zmianę!',
       price: '200zł'
     },
     {
       backgroundImage: 'url("https://media.istockphoto.com/id/1365886686/photo/shot-of-a-woman-working-out-with-a-kettle-bell.jpg?s=612x612&w=0&k=20&c=dP49_35EQgBeERghq_xnYEAvDLBXn2Vt0Xhx9rdWLl0=")',
-      title: 'Prowadzenie online',
-      shortDescription: 'Ideal for couples.',
-      longDescription: 'This plan includes 5 sessions per month, nutrition guidance, and 24/7 gym access.',
+      title: 'Współpraca online #TeamKrasiński',
+      shortDescription: 'opis krótki',
+      longDescription: 'opis długi',
       price: '300zł/msc'
     },
   ];
